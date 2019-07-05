b2844a4623f6307507c2fa6df066ab7650ec28d4
elijahmm17 <elijahmm17@gmail.com>
2019-07-05 18:03:11 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price 
       t.text :image_url 
       t.text :size 
-      # t.datetime :created_at
-      # t.datetime :updated_at
+     t.datetime :created_at
+      # t.datetime :updated_at # 
     end 
   end 
 end 
