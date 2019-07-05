4496df550f55ca607d8cc761b852cd178c1456ba
elijahmm17 <elijahmm17@gmail.com>
2019-07-05 18:12:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,12 @@
 
 class CreateHauntedHouse < ActiveRecord::Migration[4.2]
   def change 
-    create_table :haunted_houses
+    create_table :haunted_houses do |t| 
+      t.text :name 
+      t.text :location 
+      t.text :theme 
+      t.float :price 
+      t.boolean :
+    end 
   end 
 end 
