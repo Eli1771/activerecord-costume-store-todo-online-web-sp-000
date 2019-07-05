65a488277bcce2ff5e1900f85dc519d109be2d68
elijahmm17 <elijahmm17@gmail.com>
2019-07-05 18:08:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,9 +5,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_store do |t|
       t.text :name 
       t.text :location 
-      t.text :costume_inventory 
+      t.integer :costume_inventory 
       t.integer :employee_count 
-      t.boolean :in_business
+      t.boolean :still_in_business
     end 
   end 
 end 
